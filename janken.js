1f8f9c76c183930c32208b9947d76107e16bd4d8
Trace Thompson <thompsontrace13@gmail.com>
2021-05-04 20:58:55 -0700

    janken.js

--- janken.js
+++ janken.js
@@ -3,6 +3,7 @@
 const GameValues = ["rock", "paper", "scissors"];
 let PlayerPoints = 0;
 let ComputerPoints = 0;
+let RoundsPlayed = 0;
 
  //Random selector for the computer to generate a choice from the GameValues constant   
 function ComputerPlay(){
@@ -11,7 +12,7 @@ function ComputerPlay(){
   
   //these just prompt the user for a choice and then puts the computer play function in to a constant to be used elsewhere
   const PlayerSelection = prompt("enter your selection here");
-  const PlayerInput = PlayerSelection;
+  const PlayerInput = PlayerSelection.toLowerCase();
   const ComputerSelection =  ComputerPlay();
 
 //this function manages the logic between player and computer choices returning the winner
@@ -24,11 +25,13 @@ function PlayRound(PlayerSelection,ComputerSelection){
   } else if ( ComputerSelection === "rock" && PlayerInput === "scissors"){
     ComputerPoints++;
     return("computer wins with "  +ComputerSelection);
+   
   }
 
   else if (ComputerSelection === "paper" && PlayerInput === "rock") {
     ComputerPoints++;
    return ("computer wins with "  +ComputerSelection);
+   
   }
 
   else if (ComputerSelection === "scissors" && PlayerInput === "paper"){ 
@@ -57,17 +60,20 @@ let RoundResult = PlayRound(PlayerSelection,ComputerSelection);
 
 //supposed to run 5 rounds of the game, doesn't yet. think it has to do with the syntax, read this https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#while_statement
 
-function GameAutomator(){
-let i=0;
-  do {
-    i++
-    return(RoundResult);
-  } while (i<5);
-  
+function RepetitionLogic(){
+  for(let i=0; i<5; i++);
 }
-console.log(GameAutomator());
+//didn't work
+
+function GameAutomator(RepetitionLogic){
+return(PlayRound(PlayerSelection,ComputerSelection));
+}
+console.log(GameAutomator(),PlayerPoints,ComputerPoints);
+
 
 //will accept any input "used rock paper scissors": currently unresolved
-//logic always results in player winning: this has been resolved computer play was in the logic instead of the computer selection 
+//need to make it case insensitive too (COMPLETE)
+//logic always results in player winning: this has been resolved computer play was in the logic instead of the computer selection (COMPLETE)
 
 //can I just input a fucking array of choices?
+//SCOREKEEPING
