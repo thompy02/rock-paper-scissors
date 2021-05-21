844910995259f2ad028deccb25172848bdc7c6a0
Trace Thompson <thompsontrace13@gmail.com>
2021-05-20 18:01:50 -0700

    janken.js

--- janken.js
+++ janken.js
@@ -11,11 +11,18 @@ function ComputerPlay(){
   }
   
   //these just prompt the user for a choice and then puts the computer play function in to a constant to be used elsewhere
-  let PlayerInput = prompt("enter your selection here");
+  //let PlayerInput = prompt("enter your selection here");
   let ComputerSelection =  ComputerPlay();
 
+  let ScoreReporter = ("computer points are " +ComputerPoints) && ("player points are " +PlayerPoints);
+ 
+
+
 //this function manages the logic between player and computer choices returning the winner
-function PlayRound(PlayerInput,ComputerSelection){
+function PlayRound(ComputerSelection){
+
+  let PlayerInput = prompt("enter your selection here");
+
   if (ComputerSelection === PlayerInput){
     return("players have tied");
     
@@ -61,23 +68,23 @@ function PlayRound(PlayerInput,ComputerSelection){
   
 }
 
-let RoundResult = PlayRound(PlayerInput,ComputerSelection);
 
 //supposed to run 5 rounds of the game, doesn't yet. think it has to do with the syntax, read this https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#while_statement
 
 function game(){
-  for(let i=0; i<0; i++);
-   return(PlayRound(PlayerInput,ComputerSelection)); 
+ PlayRound(ComputerSelection);
+ PlayRound(ComputerSelection);
+ PlayRound(ComputerSelection);
+ PlayRound(ComputerSelection);
+ PlayRound(ComputerSelection);
 }
 
 
 console.log(game());
-
-
 //will accept any input "used rock paper scissors": currently unresolved
 //need to make it case insensitive too (Incomplete)
 //logic always results in player winning: this has been resolved computer play was in the logic instead of the computer selection (COMPLETE)
-//the gmae function isn't working but everything else is 
+//the gmae function isn't working it should probably be a button that can initiate a new instance
 
 //can I just input a fucking array of choices?
 //SCOREKEEPING
